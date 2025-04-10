e7e90208ea13aef4f187dd35cb86f01f3b7db037
nabil-el-allaoui <hassi258456@gmail.com>
2025-04-10 09:49:06 +0100

    Add hero section

--- index.php
+++ index.php
@@ -3,12 +3,25 @@ include_once 'src/components/header.php';
 ?>
 
 <main>
-    
+    <section class="hero">
+        <div class="container position-relative">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto text-center">
+                    <div class="hero__content">
+                        <h1 class="hero__title">UNE AGENCE WEB ENGAGÉE,<br>QUI TIENT SES PROMESSES.</h1>
+                        <p class="hero__subtitle">l'agence digitale specialiste des petites et moyennes entreprises</p>
+                        <p class="hero__mission">Une seule mission : mettre en lumière votre activité sur le web et transformer vos visiteurs en <span class="hero__highlight">CLIENTS</span></p>
+                        <a href="#decouvrir" class="hero__button">DÉCOUVRIR <span class="hero__button-arrow">&#8594;</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <footer class="mt-5">
     <div class="container">
-        <p>La Casa De Com. Tous droits réservés.</p>
+        <p>&copy; <?php echo date("Y"); ?> La Casa De Com. Tous droits réservés.</p>
     </div>
 </footer>
 </body>
